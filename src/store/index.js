e927e49933b99515ac02ac77ebe5b3e32c050dea
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-18 12:16:40 +0300

    feat: use create slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,25 @@
-import { createStore } from 'redux'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
 
 const initialState = { counter: 0, showCounter: true }
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state) {
+            state.counter++
+        },
+        decrement(state) {
+            state.counter--
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.payload
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter
+        },
+    }
+})
 
 const counterReducer = (state = initialState, action) => {
     switch (action.type) {
@@ -33,6 +52,14 @@ const counterReducer = (state = initialState, action) => {
     }
 }
 
-const store = createStore(counterReducer)
+const store = configureStore({
+    reducer: counterSlice.reducer
+})
+
+// const store = configureStore({
+//     reducer: {
+//         counter: counterSlice.reducer
+//     }
+// })
 
 export default store
