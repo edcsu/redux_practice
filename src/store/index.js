62c5465cf42d0cf3b41eeb7fd1011f98a4fd4c52
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-18 12:21:31 +0300

    chore: clean store file

--- src/store/index.js
+++ src/store/index.js
@@ -21,37 +21,6 @@ const counterSlice = createSlice({
     }
 })
 
-const counterReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'increment':
-            return {
-                counter: state.counter + 1,
-                showCounter: state.showCounter
-            }
-        
-        case 'increase':
-            return {
-                counter: state.counter + action.amount,
-                showCounter: state.showCounter
-            }
-        
-        case 'decrement':
-            return {
-                counter: state.counter - 1,
-                showCounter: state.showCounter
-            }
-
-        case 'toggle':
-            return {
-                showCounter: !state.showCounter,
-                counter: state.counter
-            }
-    
-        default:
-            return state;
-    }
-}
-
 const store = configureStore({
     reducer: counterSlice.reducer
 })
